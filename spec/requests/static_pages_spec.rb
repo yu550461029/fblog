19f8434f3467138cfaa39414763cc2b0f19eb9e6
yudagong <yu550461029@126.com>
2014-11-14 21:22:53 +0800

    finist static_page

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+
+  	it "should have the content 'love station'" do
+  		visit '/static_pages/home'
+  		page.should have_content("love station")
+  	end
+
+  	it "the home's title must be beautiful" do
+		visit '/static_pages/home'
+		page.should have_selector('title', 
+							:text => "love station | Home")
+	end
+  end
+end
+
+describe "StaticPages" do
+  describe "Help page" do
+
+  	it "should have the content 'help'" do
+  		visit '/static_pages/help'
+  		page.should have_content("help")
+  	end
+
+  	it "the help's title must be beautiful" do
+		visit '/static_pages/help'
+		page.should have_selector('title', 
+							:text => "love station | Help")
+	end
+  end
+end
+
+describe "StaticPages" do
+  describe "About page" do
+
+  	it "should have the content 'about'" do
+  		visit '/static_pages/about'
+  		page.should have_content("about")
+  	end
+
+  	it "the about's title must be beautiful" do
+		visit '/static_pages/about'
+		page.should have_selector('title', 
+							:text => "love station | About")
+	end
+  end
+end
